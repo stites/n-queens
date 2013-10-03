43e5c9ab4a72a981848361ac423dc50577eb9591
phillipalexander <git@phillipalexander.io>
2013-10-03 15:47:06 -0700

    Update specs so that they test all the iterations they describe.

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -3,8 +3,8 @@ describe("solvers", function() {
 
   describe('findNRooksSolution()', function(){
 
-    it('finds a valid solution for n of 0-8', function(){
-      _.range(1, 8).map(function(n){
+    it('finds a valid solution for n of 1-8', function(){
+      _.range(1, 9).map(function(n){
         var solutionBoard = new Board(findNRooksSolution(n));
         expect(solutionBoard.get('n')).to.equal(n);
         expect(solutionBoard.hasAnyRooksConflicts()).to.be.equal(false);
@@ -15,10 +15,10 @@ describe("solvers", function() {
 
   describe('countNRooksSolutions()', function(){
 
-    it('finds the number of valid solutions for n of 0-8', function(){
-      _.range(0, 8).map(function(n){
+    it('finds the number of valid solutions for n of 1-8', function(){
+      _.range(1, 9).map(function(n){
         var solutionCount = countNRooksSolutions(n);
-        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040][n];
+        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
@@ -40,7 +40,7 @@ describe("solvers", function() {
   describe('countNQueensSolutions()', function(){
 
     it('finds the number of valid solutions for n of 0-8', function(){
-      _.range(0, 8).map(function(n){
+      _.range(0, 9).map(function(n){
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
         expect(solutionCount).to.be.equal(expectedSolutionCount);
