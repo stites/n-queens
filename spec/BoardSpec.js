b838d22a6fde08113d432f14f6bfd6255641df0f
Blake West <bwest87@gmail.com>
2013-10-25 14:16:29 -0700

    Created new describe blocks for each type, and now tests are being dynamically created, so it's more modular

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -5,17 +5,21 @@ describe("Board", function() {
   };
 
   var verifyConflictTypes = function(expectedConflicts, matrix){
-    // The Board() constructor will accept a matrix and build that into a (Backbone) Board object (as defined in Board.js) 
-    var board = new Board(matrix); 
+    // The Board() constructor will accept a matrix and build that into a (Backbone) Board object (as defined in Board.js)
+    var board = new Board(matrix);
     _.map('row col rooks majorDiagonal minorDiagonal queens'.split(' '), function(conflictType){
       var conflictDetected = board['hasAny' + capitalize(conflictType) + 'Conflicts']();
       var conflictExpected = _(expectedConflicts).contains(conflictType);
-      expect(conflictDetected).to.be.equal(conflictExpected);
+      var message = conflictExpected ? 'should' : 'should not';
+
+      it(message + " find a " + conflictType + " conflict", function() {
+        expect(conflictDetected).to.be.equal(conflictExpected);
+      });
     });
   };
 
-  it("should find non conflicts", function() {
-    verifyConflictTypes([], [
+  describe("Empty board", function() {
+    verifyConflictTypes([''], [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
@@ -23,8 +27,7 @@ describe("Board", function() {
     ]);
   });
 
-// Rows go from left to right
-  it("should find row conflicts", function() {
+  describe("Board with row conflicts", function() {
     verifyConflictTypes(['row', 'rooks', 'queens'], [
       [0, 0, 0, 0],
       [1, 1, 0, 0],
@@ -33,8 +36,7 @@ describe("Board", function() {
     ]);
   });
 
-// Columns go from top to bottom
-  it("should find column conflicts", function() {
+  describe("Board with col conflicts", function() {
     verifyConflictTypes(['col', 'rooks', 'queens'], [
       [1, 0, 0, 0],
       [0, 0, 0, 0],
@@ -43,8 +45,7 @@ describe("Board", function() {
     ]);
   });
 
-// Major diagonals go from top-left to bottom-right
-  it("should find major diagonal conflicts", function() {
+  describe("Board with major diagonal conflicts", function() {
     verifyConflictTypes(['majorDiagonal', 'queens'], [
       [0, 1, 0, 0],
       [0, 0, 1, 0],
@@ -53,8 +54,7 @@ describe("Board", function() {
     ]);
   });
 
-// Minor diagonals go from top-right to bottom-left
-  it("should find minor diagonal conflicts", function() {
+  describe("Board with minor diagonal conflicts", function() {
     verifyConflictTypes(['minorDiagonal', 'queens'], [
       [0, 0, 1, 0],
       [0, 0, 0, 0],
@@ -62,5 +62,4 @@ describe("Board", function() {
       [0, 0, 0, 0]
     ]);
   });
-
 });
